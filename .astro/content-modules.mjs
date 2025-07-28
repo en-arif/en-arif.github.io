6c65979737df4c3b1d62b573cd183408ea2c796b
nurarif <nurarif0151@gmail.com>
2025-07-28 22:52:56 +0700

    update: blog

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/blog/mdx-test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmdx-test.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/mdx-test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmdx-test.mdx&astroContentModuleFlag=true")],
+["src/content/blog/writeup.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwriteup.mdx&astroContentModuleFlag=true")]]);
 		
